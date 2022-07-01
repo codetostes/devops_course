e0627be9daff53578b829990cf927746d4921472
Tostes <1codetostes@gmail.com>
2022-07-01 18:00:34 -0300

    Update vagrantfile and provision for network configs

--- ansible_lab/control-node/provision.sh
+++ ansible_lab/control-node/provision.sh
@@ -3,7 +3,7 @@ sudo yum -y install epel-release
 echo "initialization of ansible installation..."
 sudo yum -y install ansible
 cat <<EOT >> /etc/hosts
-192.168.1.2 control-node
-192.168.1.3 app01
-192.168.1.4 db01
+10.10.0.2 control-node
+10.10.0.3 app01
+10.10.0.4 db01
 EOT
