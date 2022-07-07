5dfaafa86e3c1c51de2f6dc37f6706b045cac061
Tostes <1codetostes@gmail.com>
2022-07-07 16:37:41 -0300

    Create provision file to put ssh key of control-node

---
+++ ansible_lab/db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0CHgWGj4soec9LmnISHLkuroRd/ZcJY1/4lvAUWeejwd6PSLWxgoFLY2Wp6Y5WkfyW2R7UpYP4qu3W8JPDVroq0Vke7fha7VFwk5x6teztBNLaFseLFcDGqTU4BTVrCe8UFm7/mAH756oOD5wXBmDA1ZLmknDF5MjdGry8fhGEoKZ15Z42hJYYUi6zCgTW2G8pQ7pV+7EG7yy5gLpm04WJOAGBhrOKE2LPYP8k6/vdD54IElqV3u+0mZu+spinxV/+XU5JeljMGM7VRmuYr3Yfc6vOZ9YlPCWzs/Rod0l2k3skOrkTlY8TAp9BgCjxdGYaQREt4Am8Gjsk7wktniJ vagrant@control-node
+EOT
