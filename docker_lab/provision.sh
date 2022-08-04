d310cee0c4d339ffbce17f7b418613c4c74d3d21
Tostes <1codetostes@gmail.com>
2022-08-03 21:39:25 -0300

    correct mistyping

--- docker_lab/provision.sh
+++ docker_lab/provision.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io -yum
+sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
