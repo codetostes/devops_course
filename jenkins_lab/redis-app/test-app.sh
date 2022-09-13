0950d4b281749ffe58c216966d5d900dc9facba4
Tostes <tostes@linuxmail.org>
2022-09-13 20:43:49 -0300

    updating test-app to better performance

--- jenkins_lab/redis-app/test-app.sh
+++ jenkins_lab/redis-app/test-app.sh
@@ -1,14 +1,13 @@
 #/bin/bash
 RESULT="'wget -qO- https://localhost:8090/'"
 wget -q localhost:8090
-if [ $? -eq 0 ]
+if [ $RESULT == *"Number"* ]
 then
-    echo 'ok - serviço no ar!'
-elif [[ $RESULT == *"Number"* ]]
-then
-    echo 'ok - number of visits'
     echo $RESULT
+elif [[ $? -eq 0 ]]
+then
+    echo 'Service UP, but wrong return'
 else
-    echo 'not ok - number os visits'
+    echo 'Service on ERROR'
     exit 1
 fi
